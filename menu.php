062fdfb2ba31fa9ffffa615d39f275f78e157248
Demetrio Silva <demetriofds@gmail.com>
2020-09-20 19:56:21 -0300

    Alteração de menu

--- menu.php
+++ menu.php
@@ -2,6 +2,7 @@
     <div class="modulo azul">
         <h3>13. Sessão</h3>
         <ul>
+            <li><a href="exercicio.php?dir=api&file=ler_arquivo">Ler Arquivo</a></li>
             <li><a href="exercicio.php?dir=api&file=escrever_arquivo">Escrever Arquivo</a></li>
             <li><a href="exercicio.php?dir=api&file=datas_02">Datas 02</a></li>
             <li><a href="exercicio.php?dir=api&file=datas_01">Datas 01</a></li>
